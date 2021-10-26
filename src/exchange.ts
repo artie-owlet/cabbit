0358c44170f888d294b45ed1f445ea7b371b880e
artie-owlet <artie-owlet@protonmail.com>
2021-10-26 22:24:23 +0300

    save

--- src/exchange.ts
+++ src/exchange.ts
@@ -34,6 +34,52 @@ class BaseExchange {
     }
 
     protected consumeImpl<T>(mw: ConsumeMiddleware<T>, routingKey: string, args?: IArguments): void {
-        const queue = new Queue<T>(this.client, this.parser, mw, )
+        const queue = new Queue<T>(this.client, this.parser, mw, undefined);
+        this.client.bindQueue(this.name, queue.name, routingKey, args);
     }
+
+    protected exchangeImpl<E extends BaseExchange>(name: string, exType: string, opts: IExchangeOptions | undefined,
+        routingKey: string, args?: IArguments): E {
+        let ex: BaseExchange;
+        switch (exType) {
+            case 'fanout':
+                return new FanoutExchange(this.client, this.parser, name, true, opts);
+                break;
+            case '':
+                return new Exchange(this.client, this.parser, name, true, opts);
+                break;
+            case '':
+                return new Exchange(this.client, this.parser, name, true, opts);
+                break;
+            case '':
+                return new Exchange(this.client, this.parser, name, true, opts);
+                break;
+            default:
+                return new Exchange(this.client, this.parser, name, true, opts);
+        }
+    }
+
+    // protected fanoutImpl(name: string, options: IExchangeOptions | undefined,
+    //     routingKey: string, args?: IArguments): FanoutExchange {
+    //     const e = new FanoutExchange(this.client, this.parseContent, exchange, true, options);
+    //     return this.exchangeImpl(e, routingArgs);
+    // }
+
+    // protected directImpl(name: string, options: IExchangeOptions | undefined,
+    //     routingKey: string, args?: IArguments): DirectExchange {
+    //     const e = new DirectExchange(this.client, this.parseContent, exchange, true, options);
+    //     return this.exchangeImpl(e, routingArgs);
+    // }
+
+    // protected topicImpl(name: string, options: IExchangeOptions | undefined,
+    //     routingKey: string, args?: IArguments): TopicExchange {
+    //     const e = new TopicExchange(this.client, this.parseContent, exchange, true, options);
+    //     return this.exchangeImpl(e, routingArgs);
+    // }
+
+    // protected headersImpl(name: string, options: IExchangeOptions | undefined,
+    //     routingKey: string, args?: IArguments): HeadersExchange {
+    //     const e = new HeadersExchange(this.client, this.parseContent, exchange, true, options);
+    //     return this.exchangeImpl(e, routingArgs);
+    // }
 }
